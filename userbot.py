97253b300c4a52c8462c7da5ffec4bd734639b68
Levganster <kuzmitch73son@gmail.com>
2024-10-17 18:57:09 +0300

    added settings

--- userbot.py
+++ userbot.py
@@ -1,33 +1,82 @@
 import asyncio
 from telethon import TelegramClient, events
 from googletrans import Translator
+import json
 
-API_ID = 'your_api_id'
-API_HASH = 'your_api_hash'
-PHONE_NUMBER = 'your_phone_number'
+API_ID = 'введите API ID'
+API_HASH = 'введите API HASH'
+PHONE_NUMBER = 'введите номер телефона'
 
 client = TelegramClient('userbot_session', API_ID, API_HASH)
 translator = Translator()
 
-@client.on(events.NewMessage(outgoing=True, pattern='/te'))
+@client.on(events.NewMessage(outgoing=True, pattern=r'/help'))
 async def handler(event):
+    await event.edit('/te <язык> <текст> - перевести текст на указанный язык и изменить сообщение\n/td <язык> <текст> - перевести текст на указанный язык удалить прошлое сообщение и отправить новое\n/set - показать текущие настройки\n/set <номер параметра> <значение> - установить параметр\n/help - показать это сообщение')
 
-    original_text = event.message.message[len('/te '):]
+with open('settings.json', 'r', encoding='utf-8') as f:
+    settings = json.load(f)
 
-    translation = translator.translate(original_text, src='ru', dest='en').text
+@client.on(events.NewMessage(outgoing=True, pattern=r'/set'))
+async def handler(event):
+    message_parts = event.message.message.split(' ', 2)
+    
+    if len(message_parts) == 1:
+        await event.edit(f'Текущие настройки:\n1. Добавлять оригинальное сообщение: {settings["add_original_message"]}\n2. Капитализация первой буквы: {settings["capitalize_first_letter"]}\n3. Языковые настройки: {settings["original_language"]}')
+        return
 
-    new_text = f"{original_text}\n\n{translation}"
+    name = message_parts[1]
+    param = message_parts[2]
+
+    if name == '1':
+        settings['add_original_message'] = param == 'true'
+    elif name == '2':
+        settings['capitalize_first_letter'] = param == 'true'
+    elif name == '3':
+        settings['original_language'] = param
+
+    with open('settings.json', 'w', encoding='utf-8') as f:
+        json.dump(settings, f, indent=4)
+
+    await event.edit(f'Настройки сохранены:\n1. Добавлять оригинальное сообщение: {settings["add_original_message"]}\n2. Капитализация первой буквы: {settings["capitalize_first_letter"]}\n3. Языковые настройки: {settings["original_language"]}')
+
+@client.on(events.NewMessage(outgoing=True, pattern=r'/te (\w{2})'))
+async def handler(event):
+    message_parts = event.message.message.split(' ', 2)
+    dest_lang = message_parts[1]
+    original_text = message_parts[2]
+
+    translation = translator.translate(original_text, src=settings['original_language'], dest=dest_lang).text
+
+    if settings['capitalize_first_letter']:
+        original_text = original_text.capitalize()
+        translation = translation.capitalize()
+        
+    new_text = original_text
+    if settings['add_original_message']:
+        new_text += f"\n\n{translation}"
+    else:
+        new_text = translation
 
     await event.edit(new_text)
 
-@client.on(events.NewMessage(outgoing=True, pattern='/td'))
+@client.on(events.NewMessage(outgoing=True, pattern=r'/td (\w{2})'))
 async def handler(event):
+    message_parts = event.message.message.split(' ', 2)
+    dest_lang = message_parts[1]
+    original_text = message_parts[2]
 
-    original_text = event.message.message[len('/td '):]
+    translation = translator.translate(original_text, src=settings['original_language'], dest=dest_lang).text
 
-    translation = translator.translate(original_text, src='ru', dest='en').text
+    if settings['capitalize_first_letter']:
+        original_text = original_text.capitalize()
+        translation = translation.capitalize()
 
-    new_text = f"{original_text}\n\n{translation}"
+    new_text = original_text
+    if settings['add_original_message']:
+        new_text += f"\n\n{translation}"
+    else:
+        new_text = translation
 
     await event.delete()
     await event.respond(new_text)
